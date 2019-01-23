08a300315830be8786525d16db75e4d475905223
duckhee <shldhee@yahoo.co.kr>
2019-01-23 17:53:19 +0900

    remove ContentMain

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,6 +1,17 @@
 import React, { Component } from "react";
-import ContentMain from "../ContentMain";
+import ContentBox from "../ContentBox";
 import "./Content.scss";
+
+const boxList = [
+  {
+    img: "http://placehold.it/364X243",
+    category: "travel",
+    title: "title1",
+    date: "2018-03-01",
+    like: "100",
+    comment: "5"
+  }
+];
 
 class Content extends Component {
   render() {
@@ -12,7 +23,13 @@ class Content extends Component {
       >
         <div className="content">
           <h1 className="content__title">doki's blog</h1>
-          <ContentMain />
+          <ul>
+            <li>
+              {
+                boxList.map(box => <ContentBox key={box.img} img={box.img} category={box.category} title={box.title} date={box.date} like={box.like} comment={box.comment} />)
+              }
+            </li>
+          </ul>
         </div>
       </div>
     );
--- src/components/ContentBox/ContentBox.js
+++ src/components/ContentBox/ContentBox.js
@@ -1,43 +1,55 @@
-import React, { Component } from "react";
+import React from "react";
 import "./ContentBox.scss";
 
-const boxList = [
-  {
-    img: "http://placehold.it/364X243",
-    category: "travel",
-    title: "title1",
-    date: "2018-03-01",
-    like: "100",
-    comment: "5"
-  }
-];
-
-class ContentBox extends Component {
-  render() {
-    return (
-      <>
-        {boxList.map(boxObj => (
-          <div className="box" key={boxObj.title}>
-            <a className="box__link" href="#">
-              <img className="box__img" src={boxObj.img} alt="" />
-            </a>
-            <div className="box__content">
-              <a href="#" className="box__category">
-                {boxObj.category}
-              </a>
-              <a className="box__link" href="#">
-                <h3 className="box__title">{boxObj.title}</h3>
-              </a>
-              <div className="box__status">
-                <span className="box__like">{boxObj.like}</span>
-                <span className="box__comment">{boxObj.comment}</span>
-              </div>
-            </div>
-          </div>
-        ))}
-      </>
-    );
-  }
+const ContentBox = ({ img, category, title, like, comment }) => {
+  return (
+    <div className="box" key={title}>
+      <a className="box__link" href="#">
+        <img className="box__img" src={img} alt="" />
+      </a>
+      <div className="box__content">
+        <a href="#" className="box__category">
+          {category}
+        </a>
+        <a className="box__link" href="#">
+          <h3 className="box__title">{title}</h3>
+        </a>
+        <div className="box__status">
+          <span className="box__like">{like}</span>
+          <span className="box__comment">{comment}</span>
+        </div>
+      </div>
+    </div>
+  )
 }
+
+
+// class ContentBox extends Component {
+//   render() {
+//     return (
+//       <>
+//         {boxList.map(boxObj => (
+//           <div className="box" key={boxObj.title}>
+//             <a className="box__link" href="#">
+//               <img className="box__img" src={boxObj.img} alt="" />
+//             </a>
+//             <div className="box__content">
+//               <a href="#" className="box__category">
+//                 {boxObj.category}
+//               </a>
+//               <a className="box__link" href="#">
+//                 <h3 className="box__title">{boxObj.title}</h3>
+//               </a>
+//               <div className="box__status">
+//                 <span className="box__like">{boxObj.like}</span>
+//                 <span className="box__comment">{boxObj.comment}</span>
+//               </div>
+//             </div>
+//           </div>
+//         ))}
+//       </>
+//     );
+//   }
+// }
 
 export default ContentBox;
